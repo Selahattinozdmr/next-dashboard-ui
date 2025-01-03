be6cb25fc65ca35596457ea85baa0e79bed6ace7
Selahattinozdmr <selahattin.ozdemir@asu.edu.tr>
2025-01-04 01:36:03 +0300

    add initial dashboard and authentication pages, components, and charts

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -112,3 +112,26 @@ const menuItems = [
     ],
   },
 ];
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div className=" mt-4 text-sm">
+      {menuItems.map((i) => (
+        <div className=" flex flex-col gap-2" key={i.title}>
+          <span className=" hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
+          {i.items.map((j) => (
+            <Link href={j.href} key={j.label} className=" flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
+              <Image src={j.icon} alt="" width={20} height={20} />
+              <span className=" hidden lg:block">{j.label}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Menu;
