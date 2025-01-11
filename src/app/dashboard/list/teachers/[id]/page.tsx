d2294c70f7006e12e3a041998519a8aee609d493
Selahattinozdmr <selahattin.ozdemir@asu.edu.tr>
2025-01-11 12:00:08 +0300

    add SingleTeacherPage, SubjectListPage, LessonListPage, and AnnouncementListPage components; implement pagination and table search functionality

---
+++ src/app/dashboard/list/teachers/[id]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SingleTeacherPage = () => {
+  return (
+    <div className=' flex-1 p-4 flex flex-col xl:flex-row gap-4'>
+        {/* LEFT */}
+        <div className="w-full xl:w-2/3">l</div>
+        {/* RIGHT */}
+        <div className="w-full xl:w-1/3">r</div>
+    </div>
+  )
+}
+
+export default SingleTeacherPage
